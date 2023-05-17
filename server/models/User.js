6cd824d339dab2080c97765a7755dca881986b6d
sladeutley <slade.utley@gmail.com>
2023-05-17 07:12:22 -0700

    create mongoose schema in User model

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+//this is where we use info from ERD about user
+
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true, //cannot have duplicate emails
+    },
+    password: { /* ***Usually, you would have a lot more requirements for password, so when do audio/podcast app, look into that */
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
