9b4d29719ca05d924ed09dacab0784d5688b930d
sladeutley <slade.utley@gmail.com>
2023-05-20 07:53:24 -0700

    configuring in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,28 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage"; //this is whole reason for jsconfig.json. itstead of having to put './scenes/homePage
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode); //grabs mode we have from our state 
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //sets up theme, but have to pass in material ui in ThemeProvider below in return statement
 
   return (
     <div className="app">
-      <h1>hi</h1>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          {/* resets css to basic css for material ui */}
+          <CssBaseline /> 
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
