067bc11ce375dbd90fb97c0b37029a118df94abc
sladeutley <slade.utley@gmail.com>
2023-05-20 07:13:57 -0700

    did some imports and set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage"; //this is whole reason for jsconfig.json. itstead of having to put './scenes/homePage
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
+
   return (
     <div className="app">
       <h1>hi</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
